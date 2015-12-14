5ceeaee06eeaf794674d5a39defd76af1ade70a9
Hassan Syyid <hassansyyid@gmail.com>
2015-12-14 07:40:02 -0500

    Bug fixes

--- src/main/java/io/github/hsyyid/polis/listeners/PlayerPlaceBlockListener.java
+++ src/main/java/io/github/hsyyid/polis/listeners/PlayerPlaceBlockListener.java
@@ -16,6 +16,66 @@ import org.spongepowered.api.event.block.ChangeBlockEvent;
 public class PlayerPlaceBlockListener
 {
 	@Listener
+	public void onPlayerPlaceFluid(ChangeBlockEvent.Fluid event)
+	{
+		if (event.getCause().first(Player.class).isPresent())
+		{
+			Player player = (Player) event.getCause().first(Player.class).get();
+			
+			for (Transaction<BlockSnapshot> transaction : event.getTransactions())
+			{
+				String isClaimed = ConfigManager.isClaimed(transaction.getFinal().getLocation().get());
+					
+				if(!isClaimed.equals("false"))
+				{
+					if(isClaimed.equals("SafeZone") && player.hasPermission("polis.claim.admin.modify"))
+					{
+						return;
+					}
+					
+					String playerTeamName = null;
+
+					for (String team : ConfigManager.getTeams())
+					{
+						ArrayList<String> uuids = ConfigManager.getMembers(team);
+						if (uuids.contains(player.getUniqueId().toString()))
+						{
+							playerTeamName = team;
+							break;
+						}
+						else if (ConfigManager.getExecutives(team).contains(player.getUniqueId().toString()))
+						{
+							playerTeamName = team;
+							break;
+						}
+						else if (ConfigManager.getLeader(team).equals(player.getUniqueId().toString()))
+						{
+							playerTeamName = team;
+							break;
+						}
+					}
+
+					if (playerTeamName != null)
+					{
+						if(!(isClaimed.equals(playerTeamName)))
+						{
+							player.sendMessage(Texts.of(TextColors.GREEN, "[Polis]: ", TextColors.DARK_RED, "Error! ", TextColors.RED, "This land is claimed."));
+							event.setCancelled(true);
+							return;
+						}
+					}
+					else
+					{
+						player.sendMessage(Texts.of(TextColors.GREEN, "[Polis]: ", TextColors.DARK_RED, "Error! ", TextColors.RED, "This land is claimed."));	
+						event.setCancelled(true);
+						return;
+					}
+				}
+			}
+		}
+	}
+	
+	@Listener
 	public void onPlayerPlaceBlock(ChangeBlockEvent.Place event)
 	{
 		if (event.getCause().first(Player.class).isPresent())
@@ -74,5 +134,4 @@ public class PlayerPlaceBlockListener
 			}
 		}
 	}
-
 }
