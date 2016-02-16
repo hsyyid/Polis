d4185302f1b14cedde8697a7214f0546fb2fc9fb
Hassan Syyid <hassansyyid@gmail.com>
2016-02-16 14:17:18 -0500

    Version bump

--- src/main/java/io/github/hsyyid/polis/PluginInfo.java
+++ src/main/java/io/github/hsyyid/polis/PluginInfo.java
@@ -7,6 +7,6 @@ public abstract class PluginInfo
 {
 	public static final String ID = "Polis";
 	public static final String NAME = "Polis";
-	public static final String VERSION = "2.0b";
+	public static final String VERSION = "2.0c";
 	public static final String DEPENDENCIES = "after:Updatifier";
 }
