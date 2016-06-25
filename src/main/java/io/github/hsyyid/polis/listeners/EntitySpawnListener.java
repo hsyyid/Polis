32967ff13c8e9bbea645747c7b6d389c00840835
Hassan Syyid <hassansyyid@gmail.com>
2016-06-25 14:59:17 -0400

    Minecraft 1.10

--- src/main/java/io/github/hsyyid/polis/listeners/EntitySpawnListener.java
+++ src/main/java/io/github/hsyyid/polis/listeners/EntitySpawnListener.java
@@ -15,52 +15,55 @@ public class EntitySpawnListener
 	public void onEntitySpawn(SpawnEntityEvent event)
 	{
 		event.filterEntities(entity -> {
-			if (ConfigManager.isClaimed(entity.getLocation()).equals("SafeZone") && entity instanceof Monster)
+			if (entity != null)
 			{
-				return false;
-			}
-
-			if (ConfigManager.isClaimed(entity.getLocation()).equals("SafeZone"))
-			{
-				Map<EntityType, String> protectedMobs = ConfigManager.getMobs("safezone");
-
-				if (protectedMobs.containsKey(entity.getType()))
+				if (ConfigManager.isClaimed(entity.getLocation()).equals("SafeZone") && entity instanceof Monster)
 				{
-					String option = protectedMobs.get(entity.getType());
+					return false;
+				}
 
-					if (option.equals("all"))
+				if (ConfigManager.isClaimed(entity.getLocation()).equals("SafeZone"))
+				{
+					Map<EntityType, String> protectedMobs = ConfigManager.getMobs("safezone");
+
+					if (protectedMobs.containsKey(entity.getType()))
 					{
-						return false;
-					}
-					else if (option.equals("player") && event.getCause().first(Player.class).isPresent())
-					{
-						return false;
-					}
-					else if (option.equals("non-player") && !event.getCause().first(Player.class).isPresent())
-					{
-						return false;
+						String option = protectedMobs.get(entity.getType());
+
+						if (option.equals("all"))
+						{
+							return false;
+						}
+						else if (option.equals("player") && event.getCause().first(Player.class).isPresent())
+						{
+							return false;
+						}
+						else if (option.equals("non-player") && !event.getCause().first(Player.class).isPresent())
+						{
+							return false;
+						}
 					}
 				}
-			}
-			else if (ConfigManager.isClaimed(entity.getLocation()).equals("WarZone"))
-			{
-				Map<EntityType, String> protectedMobs = ConfigManager.getMobs("warzone");
-
-				if (protectedMobs.containsKey(entity.getType()))
+				else if (ConfigManager.isClaimed(entity.getLocation()).equals("WarZone"))
 				{
-					String option = protectedMobs.get(entity.getType());
+					Map<EntityType, String> protectedMobs = ConfigManager.getMobs("warzone");
 
-					if (option.equals("all"))
+					if (protectedMobs.containsKey(entity.getType()))
 					{
-						return false;
-					}
-					else if (option.equals("player") && event.getCause().first(Player.class).isPresent())
-					{
-						return false;
-					}
-					else if (option.equals("non-player") && !event.getCause().first(Player.class).isPresent())
-					{
-						return false;
+						String option = protectedMobs.get(entity.getType());
+
+						if (option.equals("all"))
+						{
+							return false;
+						}
+						else if (option.equals("player") && event.getCause().first(Player.class).isPresent())
+						{
+							return false;
+						}
+						else if (option.equals("non-player") && !event.getCause().first(Player.class).isPresent())
+						{
+							return false;
+						}
 					}
 				}
 			}
